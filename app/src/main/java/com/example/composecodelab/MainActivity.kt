781247f7989fb202ee1bcdd0fc0778add983eac9
Oleksandr Honcharuk <oleksandr.honcharuk@cantire.com>
2022-09-26 23:21:21 +0300

    Tasks 4 - 6

--- app/src/main/java/com/example/composecodelab/MainActivity.kt
+++ app/src/main/java/com/example/composecodelab/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.composecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composecodelab.ui.theme.ComposeCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,24 +21,41 @@ class MainActivity : ComponentActivity() {
     super.onCreate(savedInstanceState)
     setContent {
       ComposeCodelabTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-          Greeting("Android")
-        }
+        MyApp()
       }
     }
   }
 }
 
 @Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
+private fun Greeting(name: String) {
+  Surface(
+    color = MaterialTheme.colors.primary,
+    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+  ) {
+    Row(modifier = Modifier.padding(24.dp)) {
+      Column(modifier = Modifier.weight(1f)) {
+        Text(text = "Hello,")
+        Text(text = name)
+      }
+      OutlinedButton(onClick = { /*TODO*/ }) {
+        Text(text = "Show more")
+      }
+    }
+  }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun MyApp(names: List<String> = listOf("World", "Compose")) {
+  Column(Modifier.padding(vertical = 4.dp)) {
+    names.forEach { name -> Greeting(name) }
+  }
+}
+
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
   ComposeCodelabTheme {
-    Greeting("Android")
+    MyApp()
   }
 }
